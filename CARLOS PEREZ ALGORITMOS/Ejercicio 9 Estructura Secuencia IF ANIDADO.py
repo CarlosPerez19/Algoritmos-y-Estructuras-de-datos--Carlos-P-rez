2578460ddfa8f5134e58150d1dacf7fc7a9201c5
CarlosPerez19 <151254507+CarlosPerez19@users.noreply.github.com>
2023-11-26 13:48:52 -0500

    Ejercicio 9 Estructura Secuencia IF ANIDADO.py

--- CARLOS PEREZ ALGORITMOS/Ejercicio 9 Estructura Secuencia IF ANIDADO.py
+++ CARLOS PEREZ ALGORITMOS/Ejercicio 9 Estructura Secuencia IF ANIDADO.py
@@ -6,13 +6,12 @@ nota3 = float (input (" Ingrese la nota de Matematica: "))
 
 promedio = (nota1 + nota2 + nota3) / 3
 
-print ("Su promedio es de",promedio,"")
 
 if promedio >= 9 and promedio <= 10:
-    print ("Usted aprobo el curso y ha obtenido una beca")
-    if promedio >= 7 and promedio <=8:
-        print ("Felicidades aprobo la materia")
-    else: 
-        print ("Usted reprobo la materia")
-else:
-   print ("Las notas estan fuera de rango")
+    print ("Usted aprobo el curso y ha obtenido una beca su promedio es de ",round (promedio,2),"")
+elif promedio >= 7 and promedio <= 8:
+    print ( "Usted aprobo el curso su promedio es de ",round(promedio,2),"")
+elif promedio < 7:
+    print ("Usted reprobo la materia su promedio es de ",round(promedio,2),"")
+else: 
+    print ("Las notas estan fuera de rango")
